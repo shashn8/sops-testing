632bbed4b8fd6f3382167a5b446563a7bc605f99
Shashidhar Naraparaju <shashidhar.naraparaju@fortanix.com>
2023-11-07 15:39:45 +0530

    tf

--- var.tf
+++ var.tf
@@ -1,19 +1,22 @@
-variable "dsm_endpoint" {
-  type    = string
-  default = "https://sdkms.fortanix.com"
-}
-
-variable "dsm_username" {
-  type    = string
-  default = "shashidhar.naraparaju@fortanix.com"
-}
-
-variable "dsm_password" {
-  type    = string
-  default = "<my_password>"
-}
-
-variable "dsm_acct_id" {
-  type    = string
-  default = "25300351-321e-4c13-bf52-3e3f62758912"
+{
+	"data": "ENC[AES256_GCM,data:t4YI4TQyrbtN5lTGh/0kThOYCxlleuGibhY1/s4DHu/ZnE/KI6g83GQNbCS6ujXRF4bxy7BuysHbPWbmxf0qCpaUfimHKRwL287TV9oYKUocadgmD2lCyierB6xABEyRGbxPvsIFYXi56DMEZ+YUq7Apc6YxCfGP+pj4KFOlJekRQkHwI45zLv1eSktgFnhtiG39vbkr09aPujOQdijOCPBw0A00oWoICB824i4CftrYau4PljcG6oES1I/XF4S4HLfxeAf04VCuG3gGr2XSKvRSdLkYuTqGCavK9+PlzsfKrbNtV7rt+7zeWLZXe/avND4X2nBDbgmBaGryL/gVLThh742Yh0BxFsGh2MzJiQGSZOdFg4U4mPxTAQCghsZzUgLKrZJESdgABqlYRiBAHOjwq/PiNIa7sLTUylqaWaWrGVhh1dFpkxhU7Ugjm7QBRuJfZy1EJq+3s+yZXEG/Glz3HyB/8h0=,iv:Q45JGXq7eoMmLPiKndXNaUSc637DXIHKMf3/3bxp9C4=,tag:F9ZQRm/q26+JWgGR3/GP5A==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"fortanixdsm": [
+			{
+				"uuid": "1aea151c-699a-4ef3-acbf-c015a943c396",
+				"created_at": "2023-11-07T10:09:06Z",
+				"enc": "Ym90RG0zVnFwMld5NUxoTkhGVnkyditPdVplcG53SWtiSUo3NWovUXVqND06VGJxbnJyQWdENFZRZDc0ODpReDlyYWdyQkRxTndURDF4ZWZvVmtBPT0="
+			}
+		],
+		"lastmodified": "2023-11-07T10:09:06Z",
+		"mac": "ENC[AES256_GCM,data:5LzuBC+B2f4E3F/KJjUxMIJYYs2YAq3Q9xbyw7d5ipFpdlTzs4BdxYlLZukGbvCq7fTyqIZSD1ObuklVX19/Qc/XwmXPt0FXE2uNVeNYgeFzLIyY33D/YOvBqVzAM32crMOQnyeMnV+Y1/ONdMSByqLt9KadtSBeX21Ow9Z71qM=,iv:pl94dxZPyRd6ea/+6//GHl6hSCPOQ+7fcSb6+oZW4n0=,tag:9RwgV0t/YE+5sSrbWeX3RA==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.8.1"
+	}
 }
