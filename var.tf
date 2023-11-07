24a74ef4150e21084fae458bcc7ce9c494b730c1
Shashidhar Naraparaju <shashidhar.naraparaju@fortanix.com>
2023-11-07 14:59:07 +0530

    initial

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "dsm_endpoint" {
+  type    = string
+  default = "https://sdkms.fortanix.com"
+}
+
+variable "dsm_username" {
+  type    = string
+  default = "shashidhar.naraparaju@fortanix.com"
+}
+
+variable "dsm_password" {
+  type    = string
+  default = "<my_password>"
+}
+
+variable "dsm_acct_id" {
+  type    = string
+  default = "25300351-321e-4c13-bf52-3e3f62758912"
+}
