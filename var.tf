24e561d2260f072888ed0751cb3aed82e8d78d9d
Shashidhar Naraparaju <shashidhar.naraparaju@fortanix.com>
2023-11-08 14:48:23 +0530

    shashi

---
+++ var.tf
@@ -0,0 +1,22 @@
+{
+	"data": "ENC[AES256_GCM,data:YC3esUbhrG9kERd3b48XajUsxriCXsWgvW4/hWN18+u8LBQoxFXT9T5dGlHPWxkJIb07yzbnwYgJa4bC5DSGYvlswqTCUNC0DIZeg7YKIXaZNDpRBWhowhqcguffQOzk9aoZ5iU1IC4c3B4VLVbSTI5qaSLwLItXevuPljtnCLVr/DrUoQHUsw0ejCi6dV+XX94qCncckf5+UJEUjKwN4shpqObeFTGp9QXLASbzYwlcYbVw1REH0LeANHv0kD5SRMDxRwlWn71Tjm9TLtsGoCd+Nd3hwyQZogyo+AQGYuleZIuyVmGTcD1tLjGwuaM5s24D8ZX1qpU9A69yR9o5CT8cniKwaX5E1xDmZXYowfTO6UrhE+E1fFUtHkOf9+bOUbrUinyi/BVCpdRkGbQDe/57MCqTBWx57mfRkGi9/dEJnfnFJ8fnFeSKXXG7Og17ofayshC4R1hLeUqd0Vv5CrEoF4nFr0w=,iv:K0VHixPfV65g88OmyGZOd9AL6uF9HPjtmFr7vzvk4MI=,tag:I/3oZ22r428Uqgy6pLLLCg==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"fortanixdsm": [
+			{
+				"uuid": "1aea151c-699a-4ef3-acbf-c015a943c396",
+				"created_at": "2023-11-08T09:17:13Z",
+				"enc": "ZVVyYk9sN0JkRFhtaHFTZ0ZPVHlZMjBwY1FDVVVVRDE2OHB4RUw3VmdWaz06bHRCM2ZYdFF6SGJBSERmRTpncGtlcGJOWlhtWmxtTVM4QjJtUEJBPT0="
+			}
+		],
+		"lastmodified": "2023-11-08T09:17:14Z",
+		"mac": "ENC[AES256_GCM,data:wdUSoKDtHlMUmncmdGU6zrBrM/Y9aEmvrR8j3Th9yk1A8pXMB63A2klCrMvpFUH7jK2rfH0TvlZr8NHCmhlmbVNvEAdlVAjNcpqmxh/egUr96uDBol6mYFwlA3KramcJyGblUJa424PABU3BTJdn9FIGw2fNJO8GVA8RkM4Q1WE=,iv:gkI4FW+zZfKVERMpaISD7x0EZaXY6ByRXV8fZSk6fjk=,tag:gIbJ+WNr94qV12ZGuf6ZZg==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.8.1"
+	}
+}
